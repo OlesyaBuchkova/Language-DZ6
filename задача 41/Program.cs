7b8126c4bff45aca2bffacf9651a91ae12727582
olesya <nearolesya@gmil.com>
2023-09-07 23:15:20 +0300

    Дз семинара 6

---
+++ задача 41/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите несколько чисел через запятую: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array  = new int [number];
+void mas(int number)
+{
+       for (int i = 0; i < number; i++) 
+    {
+        Console.WriteLine($"Введите {i+1} элемент массива");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+} 
+
+
+int Num (int [] array)
+{
+    
+    int col = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        col++;
+    }
+    return col;
+}
+
+mas(number);
+Console.WriteLine($"Количество чисел больше 0 = {Num(array)}");
---
+++ задача 43/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+Console.WriteLine("Введите координаты первой точки");
+double b1 = Convert.ToInt32(Console.ReadLine());
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки");
+double b2 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
